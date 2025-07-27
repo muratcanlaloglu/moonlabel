6d86e40d54d65a1e4cae32144ce6fd02e7c5e98d
muratcanlaloglu <quaristr@gmail.com>
2025-07-27 15:59:32 +0300

    feat: add to_coco method for converting detection coordinates to COCO format

--- backend/src/convert.py
+++ backend/src/convert.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class Detection:
     label: str
@@ -15,3 +16,10 @@ class Detection:
         height = (self.y_max - self.y_min) / image_height
         return x_center, y_center, width, height
 
+    def to_coco(self, image_height: int, image_width: int) -> tuple[int, int, int, int]:
+        x_top_left = self.x_min
+        y_top_left = self.y_min
+        bbox_width = self.x_max - self.x_min
+        bbox_height = self.y_max - self.y_min
+
+        return x_top_left, y_top_left, bbox_width, bbox_height
